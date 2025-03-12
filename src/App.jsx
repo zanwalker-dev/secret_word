cf1cec6e692f2c811a73ba1beea54a8922a54f4d
André "Zan" Rocha <132136080+zanwalker-dev@users.noreply.github.com>
2025-03-12 10:45:21 -0300

    feat: stages

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@
 import './styles/App.css';
 // React
 import { useCallback, useEffect, useState } from 'react';
-// Pages
+// Data
+import { wordsList } from './data/words';
+// Components
 import StartScreen from './components/startscreen';
 
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'},
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
 
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <StartScreen />}
+      {gameStage === 'end' && <StartScreen />}
     </div>
   );
 };
